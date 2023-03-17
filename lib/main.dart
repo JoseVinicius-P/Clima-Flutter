fd38c951f82adf0656629f43def97c1087e1f570
JoseVinicius-P <josevpf123@gmail.com>
2023-03-16 21:03:34 -0300

    feat: add background image

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Clima',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF313341)),
         useMaterial3: true,
+
+        //Tema para textos
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(fontSize: 36.0, color: Color(0xFF313341)),
+        )
       ),
       home: const SearchCity(title: 'Flutter Demo Home Page'),
     );
@@ -36,39 +41,41 @@ class _SearchCityState extends State<SearchCity> {
   Widget build(BuildContext context) {
 
     var theme = Theme.of(context);
-    var style = theme.textTheme.displaySmall!.copyWith(
-      color: Colors.white,
-    );
 
     return Scaffold(
-      backgroundColor: Colors.deepOrange.shade300,
-      body:
-      SafeArea(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
-                    child:
-                      Text(
-                        'Vamos pesquisar uma cidade...',
-                        style: style,
-                      )
-                  ),
-                  const Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: SearchTextField(),
-                        ),
-                      ],
-                    )
-                  ),
-                ],
-              )
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
           )
-
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+                child:
+                Text(
+                  'Vamos pesquisar uma cidade...',
+                  style: theme.textTheme.titleLarge,
+                )
+              ),
+              const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: SearchTextField(),
+                    ),
+                  ],
+                )
+              ),
+            ],
+          )
+        ),
+      )
     );
   }
 }
@@ -80,9 +87,12 @@ class SearchTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    var theme = Theme.of(context);
+
     return TextFormField(
       style: const TextStyle(
-        color: Colors.white,
+        color: Color(0xFF313341),
       ),
       decoration: InputDecoration(
         enabledBorder: OutlineInputBorder(
@@ -95,12 +105,12 @@ class SearchTextField extends StatelessWidget {
         ),
         prefixIcon: const Icon(
           Icons.search,
-          color: Colors.white,
+          color: Color(0xFF313341),
         ),
         hintText: "Pesquisar...",
-        hintStyle: const TextStyle(color: Colors.white),
+        hintStyle: const TextStyle(color: Color(0xFF313341)),
         filled: true,
-        fillColor: Colors.deepOrange.shade200,
+        fillColor: const Color(0x30FFFFFF),
       ),
       cursorColor: Colors.white,
     );
