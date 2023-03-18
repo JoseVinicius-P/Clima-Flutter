679c46261319612f3ca03d8d9c7a6d285c3f1dcc
JoseVinicius-P <josevpf123@gmail.com>
2023-03-17 22:36:24 -0300

    feat/refactor: added color and changed nome a color

--- lib/values/MyColors.dart
+++ lib/values/MyColors.dart
@@ -1,7 +1,8 @@
 import 'dart:ui';
 
 class MyColors {
-  static const textColor = Color(0xFF313341);
+  static const textColorPrimary = Color(0xFF313341);
   static const whiteTranparent30 = Color(0x30FFFFFF);
   static const whiteTranparent70 = Color(0x70FFFFFF);
+  static const textColorGrayLight = Color(0xFF9A938C);
 }
