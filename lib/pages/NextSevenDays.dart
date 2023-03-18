3ec8d56f31ea1f63fba83d56db5b8a081c492e3f
JoseVinicius-P <josevpf123@gmail.com>
2023-03-18 19:05:49 -0300

    style: added elements

--- lib/pages/NextSevenDays.dart
+++ lib/pages/NextSevenDays.dart
@@ -1,3 +1,4 @@
+import 'package:clima/values/MyColors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +15,9 @@ class _NextSevenDays extends State<NextSevenDays>{
   Widget build(BuildContext context) {
 
     var theme = Theme.of(context);
+    final String img_name = 'umbrella.png';
+    final String dado = '22mm';
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -34,12 +38,109 @@ class _NextSevenDays extends State<NextSevenDays>{
             fit: BoxFit.cover,
           )
         ),
-        child: Column(
-          children: [
+        child: Padding(
+          padding: EdgeInsets.only(top: 30.0, right: 30.0, left: 30.0),
+          child: Column(
+            children: [
+              SafeArea(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: MyColors.whiteTranparent70,
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(20.0)
+                      )
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              'Amanhã',
+                              style: theme.textTheme.labelMedium?.copyWith(fontSize: 15.0, fontWeight: FontWeight.bold),
+                            ),
+                            Spacer(),
+                            Text(
+                              '25º',
+                              style: theme.textTheme.labelMedium?.copyWith(fontSize: 15.0, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 5),
+                            Image(
+                              image: AssetImage('assets/images/cludy.png'),
+                              width: 60,
+                              fit: BoxFit.cover,
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 30),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ConditionWeather(img_name: 'umbrella.png', dado: '22mm', theme: theme),
+                            SizedBox(width: 30),
+                            ConditionWeather(img_name: 'wind.png', dado: '15km/h', theme: theme),
+                            SizedBox(width: 30),
+                            ConditionWeather(img_name: 'humidity.png', dado: '10%', theme: theme),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
 
-          ],
+                ],
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class ConditionWeather extends StatelessWidget {
+  const ConditionWeather({
+    super.key,
+    required this.img_name,
+    required this.dado,
+    required this.theme,
+  });
+
+  final String img_name;
+  final String dado;
+  final ThemeData theme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                  Radius.circular(12.0)
+              )
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(1.0),
+            child: Image(
+              image: AssetImage('assets/images/$img_name'),
+              width: 45,
+              fit: BoxFit.cover,
+              filterQuality: FilterQuality.high,
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          dado,
+          style: theme.textTheme.labelMedium?.copyWith(fontSize: 15.0),
+        )
+      ],
     );
   }
 }
