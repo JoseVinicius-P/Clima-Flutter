8360506627fa258c3355195fbb6b8ac66af46d92
JoseVinicius-P <josevpf123@gmail.com>
2023-03-17 22:37:28 -0300

    refactor: rename a color

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: MyStrings.clima,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: MyColors.textColor),
+        colorScheme: ColorScheme.fromSeed(seedColor: MyColors.textColorPrimary),
         useMaterial3: true,
 
         //Tema para textos
         textTheme: const TextTheme(
-          titleLarge: TextStyle(fontSize: 36.0, color: MyColors.textColor),
+          titleLarge: TextStyle(fontSize: 36.0, color: MyColors.textColorPrimary),
+          labelSmall: TextStyle(fontSize: 16.2, color: MyColors.textColorGrayLight, fontWeight: FontWeight.normal)
         ),
       ),
       home: const SearchCity(),
