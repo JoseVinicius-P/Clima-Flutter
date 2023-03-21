c693ff1397ccc0d8193994433792803cfda9515d
JoseVinicius-P <josevpf123@gmail.com>
2023-03-20 21:10:41 -0300

    feat: created class to fetch current weather from api

---
+++ lib/home/repositories/current_weather_api.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:clima/home/models/current_weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class CurrentWeatherApi {
+
+  Future<CurrentWeatherModel> fetchCurrentWeather() async{
+    final response = await http
+        .get(Uri.parse('https://api.open-meteo.com/v1/forecast?latitude=-14.10&longitude=-46.63&daily=precipitation_sum,precipitation_probability_max&current_weather=true&timezone=America%2FSao_Paulo'));
+
+    if(response.statusCode == 200){
+      final decodedJson = jsonDecode(response.body);
+      print(jsonEncode(decodedJson['current_weather']));
+      return CurrentWeatherModel.fromJson(decodedJson['current_weather']);
+    }else{
+      throw Exception('Failed to load album');
+    }
+  }
+}
