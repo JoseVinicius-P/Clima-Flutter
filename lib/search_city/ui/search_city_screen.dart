1ec1e07b80daca7fcfa3c6abf9e00c57f43f0182
JoseVinicius-P <josevpf123@gmail.com>
2023-03-22 23:14:42 -0300

    refactor: reusing code of image of background

--- lib/search_city/ui/search_city_screen.dart
+++ lib/search_city/ui/search_city_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/search_city/ui/widgets/search_textfield_widget.dart';
 import 'package:clima/search_city/ui/widgets/button_next_widget.dart';
+import 'package:clima/shared/ui/widgets/background_widget.dart';
 import 'package:clima/values/MyStrings.dart';
 import 'package:flutter/material.dart';
 
@@ -21,61 +22,56 @@ class _SearchCityScreenState extends State<SearchCityScreen> {
     return Scaffold(
       //Propriedade impede fundo de se mover quando reclado é ativo
       resizeToAvoidBottomInset: false,
-      body: Container(
-        //Definido background
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/background.jpg'),
-              //Para impedir distorção da imagem
-              fit: BoxFit.cover,
-            )
-        ),
-        //SafeArea para impedir que conteudo fique em cima de elementos da tela como status bar
-        child: SafeArea(
-          //Conteudo da tela
-          child: Column(
-            children: [
-              //Definindo espaçamento entre texto e bordas da tela
-              Padding(
-                  padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
-                  child:
-                  Text(
-                    //Variavel definida no arquivo my_strings
-                    MyStrings.vamosPesquisarSuaCidade,
-                    //Tema definido no main
-                    style: theme.textTheme.titleLarge,
-                  )
-              ),
-              const Center(
-                  child: Column(
-                    //alinhando eelementos no topo
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Padding(
-                        //definindo espaço entre bordas e elemento
-                        padding: EdgeInsets.all(20.0),
-                        //adicionando text fild de pesquisa personalizado
-                        child: SearchTextFieldWidget(),
-                      ),
-                    ],
-                  )
-              ),
-              //para ocupar espaço da tela e manter o botão alinhado abaixo
-              const Expanded(
+      body: Stack(
+        children: [
+          BackgroundWidget(),
+          //SafeArea para impedir que conteudo fique em cima de elementos da tela como status bar
+          SafeArea(
+            //Conteudo da tela
+            child: Column(
+              children: [
+                //Definindo espaçamento entre texto e bordas da tela
+                Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+                    child:
+                    Text(
+                      //Variavel definida no arquivo my_strings
+                      MyStrings.vamosPesquisarSuaCidade,
+                      //Tema definido no main
+                      style: theme.textTheme.titleLarge,
+                    )
+                ),
+                const Center(
+                    child: Column(
+                      //alinhando eelementos no topo
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          //definindo espaço entre bordas e elemento
+                          padding: EdgeInsets.all(20.0),
+                          //adicionando text fild de pesquisa personalizado
+                          child: SearchTextFieldWidget(),
+                        ),
+                      ],
+                    )
+                ),
+                //para ocupar espaço da tela e manter o botão alinhado abaixo
+                const Expanded(
                   child: Align(
                     // alinhando no centro na parte inferior
                     alignment: Alignment.bottomCenter,
-                      child: Padding(
-                        //definindo espaço entre bordas e elemento
-                        padding: EdgeInsets.all(20.0),
-                        //adicionando button de pesquisa personalizado
-                        child: ButtonNextWidget(),
-                      ),
+                    child: Padding(
+                      //definindo espaço entre bordas e elemento
+                      padding: EdgeInsets.all(20.0),
+                      //adicionando button de pesquisa personalizado
+                      child: ButtonNextWidget(),
+                    ),
                   ),
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
