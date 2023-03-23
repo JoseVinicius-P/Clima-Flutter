ff67ab173e32e113862eb76d33bab44acd4133e0
JoseVinicius-P <josevpf123@gmail.com>
2023-03-22 23:18:12 -0300

    refactor: reusing code of image of background

--- lib/next_seven_days/ui/next_seven_days_screen.dart
+++ lib/next_seven_days/ui/next_seven_days_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:clima/next_seven_days/ui/widgets/condition_weather_widget.dart';
 import 'package:clima/next_seven_days/ui/widgets/day_of_week_widget.dart';
+import 'package:clima/shared/ui/widgets/background_widget.dart';
 import 'package:flutter/material.dart';
 
 //Classe que é a tela NextSevenDaysScreen, ou Teka dos próximos 7 dias
@@ -34,84 +35,79 @@ class _NextSevenDays extends State<NextSevenDaysScreen>{
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Container(
-        //Definindo background que ocupa toda a tela
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background.jpg'),
-            fit: BoxFit.cover,
-          )
-        ),
-        child: Padding(
-          //Definindo espaçamento entre bordas da tela e elementos
-          padding: EdgeInsets.only(top: 30.0, right: 30.0, left: 30.0),
-          child: Column(
-            children: [
-              //Impedindo que elementos fiquem em local errado (statusbar)
-              SafeArea(
-                child: Container(
-                  //Criando container com dados de amanhã
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.7),
-                    borderRadius: BorderRadius.all(
-                        Radius.circular(20.0)
+      body: Stack(
+        children: [
+          BackgroundWidget(),
+          Padding(
+            //Definindo espaçamento entre bordas da tela e elementos
+            padding: EdgeInsets.only(top: 30.0, right: 30.0, left: 30.0),
+            child: Column(
+              children: [
+                //Impedindo que elementos fiquem em local errado (statusbar)
+                SafeArea(
+                  child: Container(
+                    //Criando container com dados de amanhã
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.7),
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(20.0)
+                      ),
                     ),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              'Amanhã',
-                              style: theme.textTheme.labelMedium?.copyWith(fontSize: 15.0, fontWeight: FontWeight.bold),
-                            ),
-                            //Adiiconado spacer para ocupar o espaço total entre o primeiro elemento
-                            //e outros, permintindo que os proximo elementos fiquem alinhados a direita
-                            Spacer(),
-                            Text(
-                              '25º',
-                              style: theme.textTheme.labelMedium?.copyWith(fontSize: 15.0, fontWeight: FontWeight.bold),
-                            ),
-                            //Adiicionando espaçamento
-                            SizedBox(width: 5),
-                            //Definindo imagem relacionada ao tempo
-                            Image(
-                              image: AssetImage('assets/images/cludy.png'),
-                              width: 60,
-                              fit: BoxFit.cover,
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 30),
-                        Row(
-                          //Alinhando todos os filhos no centro
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ConditionWeatherWidget(img_name: 'umbrella.png', dado: '22mm'),
-                            SizedBox(width: 30),
-                            ConditionWeatherWidget(img_name: 'wind.png', dado: '15km/h'),
-                            SizedBox(width: 30),
-                            ConditionWeatherWidget(img_name: 'humidity.png', dado: '10%'),
-                          ],
-                        )
-                      ],
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'Amanhã',
+                                style: theme.textTheme.labelMedium?.copyWith(fontSize: 15.0, fontWeight: FontWeight.bold),
+                              ),
+                              //Adiiconado spacer para ocupar o espaço total entre o primeiro elemento
+                              //e outros, permintindo que os proximo elementos fiquem alinhados a direita
+                              Spacer(),
+                              Text(
+                                '25º',
+                                style: theme.textTheme.labelMedium?.copyWith(fontSize: 15.0, fontWeight: FontWeight.bold),
+                              ),
+                              //Adiicionando espaçamento
+                              SizedBox(width: 5),
+                              //Definindo imagem relacionada ao tempo
+                              Image(
+                                image: AssetImage('assets/images/cludy.png'),
+                                width: 60,
+                                fit: BoxFit.cover,
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 30),
+                          Row(
+                            //Alinhando todos os filhos no centro
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ConditionWeatherWidget(img_name: 'umbrella.png', dado: '22mm'),
+                              SizedBox(width: 30),
+                              ConditionWeatherWidget(img_name: 'wind.png', dado: '15km/h'),
+                              SizedBox(width: 30),
+                              ConditionWeatherWidget(img_name: 'humidity.png', dado: '10%'),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              //Adiiconando dados dos outros dias da semana
-              SizedBox(height:30),
-              DayOfWeekWidget(dayWeek: 'Segunda', temperature: '25º', img_name: 'cludy.png'),
-              SizedBox(height:10),
-              DayOfWeekWidget(dayWeek: 'Terça', temperature: '22º', img_name: 'cludy.png'),
-              SizedBox(height:10),
-              DayOfWeekWidget(dayWeek: 'Quarta', temperature: '30º', img_name: 'cludy.png'),
-            ],
+                //Adiiconando dados dos outros dias da semana
+                SizedBox(height:30),
+                DayOfWeekWidget(dayWeek: 'Segunda', temperature: '25º', img_name: 'cludy.png'),
+                SizedBox(height:10),
+                DayOfWeekWidget(dayWeek: 'Terça', temperature: '22º', img_name: 'cludy.png'),
+                SizedBox(height:10),
+                DayOfWeekWidget(dayWeek: 'Quarta', temperature: '30º', img_name: 'cludy.png'),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
