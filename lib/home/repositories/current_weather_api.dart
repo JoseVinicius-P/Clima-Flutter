36681fbaef8505e21a939e1abe813f8770e6302e
JoseVinicius-P <josevpf123@gmail.com>
2023-04-03 21:58:50 -0300

    feat: Ensuring data is loaded even with slow connection

--- lib/home/repositories/current_weather_api.dart
+++ lib/home/repositories/current_weather_api.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:clima/home/models/current_weather_model.dart';
 import 'package:http/http.dart' as http;
@@ -7,13 +8,17 @@ class CurrentWeatherApi {
 
   Future<CurrentWeatherModel> fetchCurrentWeather({required double latitude, required double longitude, required String timezone}) async{
     final response = await http
-        .get(Uri.parse('https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&daily=precipitation_sum,precipitation_probability_max&current_weather=true&timezone=$timezone'));
+        .get(Uri.parse('https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&daily=precipitation_sum,precipitation_probability_max&current_weather=true&timezone=$timezone'))
+        .timeout(const Duration(seconds: 10),
+        onTimeout: (){
+          return http.Response('Time out', 408);
+        });
 
     if(response.statusCode == 200){
       final decodedJson = jsonDecode(response.body);
       return CurrentWeatherModel.fromJson(decodedJson);
     }else{
-      throw Exception('Failed to load current weather');
+      return fetchCurrentWeather(latitude: latitude, longitude: longitude, timezone: timezone);
     }
   }
 }
--- lib/home/repositories/hourly_weather_api.dart
+++ lib/home/repositories/hourly_weather_api.dart
@@ -7,13 +7,17 @@ class HourlyWeatherApi {
 
   Future<HourlyWeatherModel> fetchHourlyWeather({required double latitude, required double longitude, required String timezone}) async {
     final response = await http
-        .get(Uri.parse('https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&hourly=temperature_2m,weathercode&forecast_days=2&timezone=$timezone'));
+        .get(Uri.parse('https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&hourly=temperature_2m,weathercode&forecast_days=2&timezone=$timezone'))
+        .timeout(const Duration(seconds: 10),
+        onTimeout: (){
+          return http.Response('Time out', 408);
+        });
 
     if(response.statusCode == 200){
       final decodedJson = jsonDecode(response.body);
       return HourlyWeatherModel.fromJson(decodedJson);
     }else{
-      throw Exception('Failed to load hourly weather');
+      return fetchHourlyWeather(latitude: latitude, longitude: longitude, timezone: timezone);
     }
   }
 }
